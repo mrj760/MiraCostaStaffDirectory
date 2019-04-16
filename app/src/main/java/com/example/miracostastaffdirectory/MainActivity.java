65d53e6e34afeb1b016393935ecfee134d09e9d9
micah <mj2carlsbad@gmail.com>
2019-04-16 14:49:45 -0700

    JSONLoader finished and one JSON file renamed

--- app/src/main/java/com/example/miracostastaffdirectory/MainActivity.java
+++ app/src/main/java/com/example/miracostastaffdirectory/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.miracostastaffdirectory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.miracostastaffdirectory.Model.JSONLoader;
+import com.example.miracostastaffdirectory.Model.StaffMember;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String TAG = "MCStaffDirectory";
+    private List<StaffMember> allSMList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+             allSMList = JSONLoader.loadJSONFromAsset(this);
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        }
+
+
+
+
+
+
     }
 }
